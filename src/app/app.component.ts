3e54b94456f1e8200583d090ead885d467bf6c08
zombaki <piyush.nath.2272@gmail.com>
2020-01-09 10:56:42 -0600

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+const ELEMENT_DATA: any[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'}
+];
 export class AppComponent  {
   name = 'Paste it';
+
+   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+  
   data(event:ClipboardEvent) {
     let clipboardData = event.clipboardData;
   let pastedText = clipboardData.getData('text');
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {MatTableModule} from '@angular/material/table';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,MatTableModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
