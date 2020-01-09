5ae72fcc3c4ef1eb05db040d4871850c4a3e9803
zombaki <piyush.nath.2272@gmail.com>
2020-01-09 10:58:59 -0600

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
-
+const ELEMENT_DATA: any[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'}
+];
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-const ELEMENT_DATA: any[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'}
-];
+
 export class AppComponent  {
   name = 'Paste it';
 
    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
-  
+
   data(event:ClipboardEvent) {
     let clipboardData = event.clipboardData;
   let pastedText = clipboardData.getData('text');
