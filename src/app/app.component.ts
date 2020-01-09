43e2e343a0617a94e17a941b5ddb63f2f3268992
zombaki <piyush.nath.2272@gmail.com>
2020-01-09 11:16:52 -0600

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,18 @@ const ELEMENT_DATA: any[] = [
 
 export class AppComponent  {
   name = 'Paste it';
-
-   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  val:any;
+   displayedColumns: string[] ;//= ['position', 'name', 'weight', 'symbol'];
+  dataSource: any[]; //= ELEMENT_DATA;
 
   data(event:ClipboardEvent) {
     let clipboardData = event.clipboardData;
   let pastedText = clipboardData.getData('text');
   console.log(pastedText);
+  let row_data = pastedText.split('\n');
+  this.displayedColumns = row_data[0].split('\t');
+  // Create table dataSource
+
+  this.dataSource = row_data[1].split('\t');
   }
 }
